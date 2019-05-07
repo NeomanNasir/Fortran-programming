638f49a39f373dd31034e398afffdf6b24259abb
Nasir <nnnasir30@gmail.com>
2019-05-07 22:14:18 +0600

    Add files via upload

---
+++ Numerical analysis by fortran/1a_euler_method.f90
@@ -0,0 +1,25 @@
+
+program euler_method
+    implicit none
+
+    integer :: i, n
+    real :: x0, xn, y0, h, f, y1, a
+    x0 = 0
+    y0 = 1
+    xn = 0.2
+    n = 50
+    h = (xn - x0)/n
+
+    a = x0
+    do i = 1, n
+        y1 = y0 + h*log(x0 + y0)
+        print *, x0, y1
+        x0 = x0 + h
+        y0 = y1
+    end do
+
+    print *, 'aproximate y by euler method = ', y1
+
+end program euler_method
+
+
